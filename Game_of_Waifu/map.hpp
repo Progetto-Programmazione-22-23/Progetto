d3046abc4f696c920ca0dfecd01e27ab8aef5652
RikiMuzzi <riccardo.muzzi02@gmail.com>
2023-09-10 02:06:00 +0200

    io quando lei è troppo cute: UwU

--- Game_of_Waifu/map.hpp
+++ Game_of_Waifu/map.hpp
@@ -13,12 +13,15 @@ void SpawnStart(WINDOW * win, int h);          // base di inizio di ogni livello
 void SpawnEnd(WINDOW * win, int h, int l);            // base di fine di ogni livello, porta al successivo;
 
 void SpecialBlock(WINDOW * win, int h, int l);        // pezzo di strada dritta con blocco speciale;
-void SpawnTrap(WINDOW * win, int h, int l);
-void SpawnHelp(WINDOW * win, int h, int l);
+void SpawnTrap();
+void SpawnHelp();
+void Heal();
+void Money();
+void Bomb();
 
 void GoUp(WINDOW * win, int h, int l);
 void GoStraight(WINDOW * win, int h, int l);
 void GoDown(WINDOW * win, int h, int l);
 void mapgenerator(WINDOW * win);
 
-void mapSave();
+void mapSave(int, int, char);
